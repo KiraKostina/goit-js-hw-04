e1794e7c864c7fa41cf902da2eed8c459f106475
KiraKostina <kostina.kira@gmail.com>
2024-01-31 19:30:17 +0200

    prepare c.log

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,10 @@
-function filterArray(numbers, value) { 
-    const arrayOfNumbers = [];
-    for (let i = 0; i < numbers.length; i += 1) {
-        if (numbers[i] > value) {
-            arrayOfNumbers.push(numbers[i]);
-        }
-    }
-        return arrayOfNumbers
-}
 
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
